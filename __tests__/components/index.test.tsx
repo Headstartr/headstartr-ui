3de8a6155d58ecf439a79937db68a20904b88d67
princerajcs <rajprince.cs@gmail.com>
2023-05-04 21:28:13 +0530

    p2p to P2P

--- __tests__/components/index.test.tsx
+++ __tests__/components/index.test.tsx
@@ -7,7 +7,7 @@ describe('Home', () => {
     render(<Home />);
 
     const heading = screen.getByRole('heading', {
-      name: /p2p startup marketplace/i,
+      name: /P2P startup marketplace/i,
     });
 
     expect(heading).toBeInTheDocument();
